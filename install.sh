590ac211ad44e8b867c29003d1090850ac23a289
Christopher Rowe <chris.rowe19@outlook.com>
2023-12-22 23:57:13 +0000

    Fixes install script.

--- install.sh
+++ install.sh
@@ -1,18 +1,28 @@
 #!/bin/bash
 
-script_folder="$(readlink -f "${BASH_SOURCE}")"
+script_folder="$(dirname "$(readlink -f "${BASH_SOURCE}")")"
+
 install_location="$1"
-if ! [ -z "install_location" ]; then
-	install_location="$(readlink -f "$install_location")"
+if ! [ -z "$install_location" ]; then
+    install_location="$install_location"
 else
-	install_location="$script_folder"
+#    install_location="$(readlink -f ./)"
+    install_location="./"
 fi
 install_location_argument="-o \"$install_location\""
+#echo "$install_location_argument"
+#exit
+dotnet publish "$script_folder/src/src.sln" -c Release --no-self-contained $install_location_argument
+echo "Installed at \"$(readlink -f "$install_location")\""
 
-dotnet publish "$script_folder/src/src.sln" -c Release --no-self-contained $install_location
 cp "$script_folder/TEMPLATE_run.sh" "$install_location/run.sh"
 chmod +x "$install_location/run.sh"
 
-echo "$(cat ~/.bashrc)
+if [ $(cat ~/.bashrc | grep "alias server-stats" | wc -l) -eq 0 ]; then
+    echo "$(cat ~/.bashrc)
 
-alias =$install_location/run.sh" > ~/.bashrc
+# live-compte-stats server activation alias
+alias server-stats=$(readlink -f $install_location)/run.sh" > ~/.bashrc
+
+    echo "Run \"server-stats\" to launch the application. You will need to restart you current terminal session for this to take effect."
+fi
