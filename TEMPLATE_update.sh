ed798235afd8b9d9f330fe32205a51fd92c30320
Christopher Rowe <chris.rowe19@outlook.com>
2023-12-23 00:42:00 +0000

    Removes redundant passing of path.

--- TEMPLATE_update.sh
+++ TEMPLATE_update.sh
@@ -5,7 +5,7 @@ cd "$(dirname "$(readlink -f "${BASH_SOURCE}")")"
 git clone https://github.com/QuasarX1/live-compute-stats.git /tmp/live-compute-stats
 chmod +x /tmp/live-compute-stats/install.sh
 
-/tmp/live-compute-stats/install.sh ./
+/tmp/live-compute-stats/install.sh
 
 echo "
 Cleaning up temp files..."
